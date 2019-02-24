71ec6106416e68c15b23fc69a60824ba413529d7
Nazmus Shakib <44675374+shakib500@users.noreply.github.com>
2019-02-24 18:01:15 +0600

    Update javascript.js
    working on countdown function

--- javascript.js
+++ javascript.js
@@ -1,21 +1,45 @@
 var playing = false;
 var score;
+var action;
+var timeremaining;
 // if we click star/reset button
 document.getElementById('startreset').onclick = function() {
   // if playing
-  if (playing == true) {
+  if (playing) {
     location.reload(); // reload page
-  } else { // if not playing
+  } else {// if not playing
+    //change mode to playing
     playing = true;
     score = 0;
     document.getElementById('scorevalue').innerHTML = score;
+    // show countdown box
     document.getElementById('timeremaining').style.display = 'block';
+    timeremaining = 60;
+    document.getElementById('timeremainingvalue').innerHTML = timeremaining;
     // change button to reset
-
     document.getElementById('startreset').innerHTML = 'Reset Game';
+    // start countdown
+    startCountDown();
+
+
+
 
   }
+}
 
+function startCountDown(){
+  action = setInterval(function(){
+    timeremaining -= 1;
+    document.getElementById('timeremainingvalue').innerHTML = timeremaining;
+    if(timeremaining == 0){
+      // game over
+      stopCountDown();
+    }
+  },1000);
+}
+
+function stopCountDown(){
+  clearInterval(action);
 }
 
 // reload
