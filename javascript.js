a7e92eb03ece6e39f62fef8f91679a4f0feb4efd
Nazmus Shakib <44675374+shakib500@users.noreply.github.com>
2019-02-24 17:48:02 +0600

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,14 +1,19 @@
-var playing = False;
+var playing = false;
 var score;
 // if we click star/reset button
 document.getElementById('startreset').onclick = function() {
   // if playing
-  if (playing) {
+  if (playing == true) {
     location.reload(); // reload page
   } else { // if not playing
+    playing = true;
     score = 0;
     document.getElementById('scorevalue').innerHTML = score;
     document.getElementById('timeremaining').style.display = 'block';
+    // change button to reset
+
+    document.getElementById('startreset').innerHTML = 'Reset Game';
+
   }
 
 }
