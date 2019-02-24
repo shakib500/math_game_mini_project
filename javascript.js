fe038f7cd4ba9f51e3332690fe2faa87c23e3d5d
Nazmus Shakib <44675374+shakib500@users.noreply.github.com>
2019-02-24 17:34:23 +0600

    working on start/reset button action

---
+++ javascript.js
@@ -0,0 +1,19 @@
+var playing = False;
+var score;
+// if we click star/reset button
+document.getElementById('startreset').onclick = function() {
+  // if playing
+  if (playing) {
+    location.reload(); // reload page
+  } else { // if not playing
+    score = 0;
+    document.getElementById('scorevalue').innerHTML = score;
+  }
+
+}
+
+
+// reload
+//if not playing
+// show countdown
+// reduce time by 1 second
