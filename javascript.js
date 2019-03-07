3cbb53dffa8486e2d0301211dd29a7a6ba9d4435
Nazmus Shakib <44675374+shakib500@users.noreply.github.com>
2019-03-07 23:32:07 +0600

    working on wrong answer

--- javascript.js
+++ javascript.js
@@ -8,7 +8,7 @@ document.getElementById('startreset').onclick = function() {
   // if playing
   if (playing) {
     location.reload(); // reload page
-  } else {// if not playing
+  } else { // if not playing
     //change mode to playing
     playing = true;
     score = 0;
@@ -30,53 +30,59 @@ document.getElementById('startreset').onclick = function() {
   }
 }
 
-function startCountDown(){
-  action = setInterval(function(){
+function startCountDown() {
+  action = setInterval(function() {
     timeremaining -= 1;
     document.getElementById('timeremainingvalue').innerHTML = timeremaining;
-    if(timeremaining == 0){
+    if (timeremaining == 0) {
       // game over
       stopCountDown();
 
-    show('gameOver');
-    document.getElementById('gameOver').innerHTML = '<p>Game Over!</p><p>Your Score is  ' + score + '.</p>'
-    hide('timeremaining');
-    hide('correct');
-    hide('wrong');
-    playing = false;
-    document.getElementById('startreset').innerHTML = 'Start Game';
-    //hide('gameOver');
+      show('gameOver');
+      document.getElementById('gameOver').innerHTML = '<p>Game Over!</p><p>Your Score is  ' + score + '.</p>'
+      hide('timeremaining');
+      hide('correct');
+      hide('wrong');
+      playing = false;
+      document.getElementById('startreset').innerHTML = 'Start Game';
+      //hide('gameOver');
     }
-  },1000);
+  }, 1000);
 }
 
-function stopCountDown(){
+function stopCountDown() {
   clearInterval(action);
 }
 
 // To hide an element
-function hide(id){
+function hide(id) {
   document.getElementById(id).style.display = 'none';
 }
 // To show an element
-function show(id){
+function show(id) {
   document.getElementById(id).style.display = 'block';
 }
 
 // for generating Question and multiple Answer
-function generateQA(){
-  var x = 1 + Math.round(19 * Math.random());
-  var y = 1 + Math.round(19 * Math.random());
-  correctAnswer = x*y;
-  document.getElementById('question').innerHTML = x +' X ' + y;
+function generateQA() {
+  var x = 1 + Math.round(9 * Math.random());
+  var y = 1 + Math.round(9 * Math.random());
+  correctAnswer = x * y;
+  document.getElementById('question').innerHTML = x + ' X ' + y;
   var correctPosition = 1 + Math.round(3 * Math.random());
   // filling 1 box with the correct answer
   document.getElementById('box' + correctPosition).innerHTML = correctAnswer;
 
-  for( i=1; i<5; i++){
-    if ( i !== correctPosition){
-      var wrongAnswer = (1 + Math.round(19 * Math.random())) * (1 + Math.round(19 * Math.random())); // a wrong answer
-      document.getElementById("box"+i).innerHTML = wrongAnswer;
+  var answers = [correctAnswer];
+
+  for (i = 1; i < 5; i++) {
+    if (i != correctPosition) {
+      var wrongAnswer;
+      do {
+        wrongAnswer = (1 + Math.round(9 * Math.random())) * (1 + Math.round(9 * Math.random())); // a wrong answer
+        document.getElementById("box" + i).innerHTML = wrongAnswer;
+      }
+      while (answers.indexOf(wrongAnswer) > -1)
     }
   }
 }
